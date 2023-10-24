fc23750fab8ba8679b1c8516f0a78f11e0b1cde4
Vinícius Pelegrineli <vinipelebom2@gmail.com>
2023-10-23 21:04:36 -0300

    Removing variables from function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /* Conforme explicado no enunciado adicione as palavras reservadas let ou const nas variaveis */
 
-const distance = 25000;
-const time = 710;
-const gender = "male";
-const pushUps = 40;
-const abs = 35;
+let distance = 25000;
+let time = 710;
+let gender = "male";
+let pushUps = 40;
+let abs = 35;
 
 /* 
 
@@ -24,32 +24,20 @@ function testTAF(distance, time, gender, pushUps, abs) {
 
   //Sua logica deve ser inserida aqui:
 
-  // Inicializando os limites inferiores/superiores dos valores de acordo com o gênero MASCULINO
-  let distanceCap = 24000;
-  let timeCap = 720; // 12 minutos * 60 segundos
-  let pushUpsCap = 30;
-  let absCap = 35;
-
-  // Verificação 1
-  // Caso o gênero seja FEMININO, os limites precisam ser ajustados
-  if(gender === "female"){
-    timeCap = 840; // 14 minutos * 60 segundos
-    pushUpsCap = 20;
-    absCap = 30;
+  // Verificando se a distância mínima foi alcançada pela pessoa
+  if(distance >= 24000){
+    // Verificando se o gênero é masculino
+    if(gender === "male"){
+      // Verificando se as exigências mínimas para aqueles do gênero masculino foram compridas
+      if(time < 720 && pushUps >= 30 && abs >= 35)
+        passed = true;
+    // Verificando se o gênero é feminino
+    }else if(gender === "female"){
+      // Verificando se as exigências mínimas para aqueles do gênero feminino foram compridas
+      if(time < 840 && pushUps >= 20 && abs >= 30)
+        passed = true;
+    }
   }
-
-  // Verificação 2
-  // Caso os valores dados estejam dentro do esperado de acordo com o gênero da pessoa: ela PASSA!
-  // Caso contrário: ela NÃO PASSA!
-     // A distância percorrida precisa ser PELO MENOS (>=) 2400 metros
-  if(distance >= distanceCap &&
-      // O tempo levado para percorrer a distância precisa ser MENOR (<) que 12 ou 14 minutos
-      time < timeCap &&
-      // A quantidade MÍNIMA (Inclui o valor em si, então usa-se: >=) de flexões precisa ser 30 ou 20
-      pushUps >= pushUpsCap &&
-      // A quantidade MÍNIMA (Inclui o valor em si, então usa-se: >=) de abdominais precisa ser 35 ou 30
-      abs >= absCap)
-    passed = true;
 
   // Retorno da variavel passed onde para o candidato passar ele deve ter o valor de: true
   return passed;
